e597b2ef72ab720b10ad3ec7a641b51ad3b860a6
songsari <sangjun_123@naver.com>
2017-09-22 07:36:27 +0900

    OSX Fuzzer

--- OSXfuzz/main.cpp
+++ OSXfuzz/main.cpp
@@ -6,19 +6,27 @@
 //  Copyright © 2017년 송상준. All rights reserved.
 //
 
-
+#include "define.h"
 #include "parsing.h"
 #include "fuzz.h"
 
-int main(int argc, const char * argv[]) {
+void help();
+
+int main(int argc,char *argv[]) {
     kern_return_t err;
     
     char *pData;
     char *ptr;
     char *pService[8192];
     int i = 0;
+    int select;
     pData = parsing_main();
     
+    
+    if(argc < 2) {
+        help();
+        exit(0);
+    }
     // parsing kextstat result
     ptr = strtok(pData,"\n");
     while (ptr != NULL)
@@ -27,11 +35,35 @@ int main(int argc, const char * argv[]) {
         ptr = strtok(NULL, "\n");
         i++;
     }
-
-    for(i=0;i<200;i++) {
-        userclient_fuzz(pService[i]);
+    
+    select = atoi(argv[1]);
+    
+    switch(select) {
+            
+        case 0: //newUserClient argument fuzz
+            for(i=0;i<200;i++) {
+                userclient_fuzz(pService[i]);
+            }
+            break;
+            
+        case 1:
+            //TODO
+            break;
+            
+        default:
+            printf("[-] Invalid argument\n");
+            break;
+            
     }
-
     return 0;
+    
+}
+
+
+void help() {
+    printf("usage: OSXfuzz [Type]\n");
+    printf("Type:           0 : newUserClient Fuzz\n");
+    printf("                1 : IOConnectCall Fuzz\n");
+
     
 }
