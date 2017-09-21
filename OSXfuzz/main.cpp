789c02972becfa741144f93c6261abe5ed7dd9da
songsari <sangjun_123@naver.com>
2017-09-21 23:50:04 +0900

    Initial Commit

---
+++ OSXfuzz/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OSXfuzz
+//
+//  Created by 송상준 on 2017. 9. 21..
+//  Copyright © 2017년 송상준. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
